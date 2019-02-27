29e290a2a5f146db1e40066b10b0db59d35bce3e
Dimitris Katikaridis <dkatikaridis@gmail.com>
2019-02-27 22:15:14 +0200

    build skeletal and show image

--- KinectSetupDev/KinectSetupDev/MainWindow.xaml.cs
+++ KinectSetupDev/KinectSetupDev/MainWindow.xaml.cs
@@ -13,216 +13,214 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Kinect;
+using Coding4Fun.Kinect.Wpf;
 namespace KinectSetupDev
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
-        const float MaxDepthDistance = 4095; // max value returned
-        const float MinDepthDistance = 850; // min value returned
-        const float MaxDepthDistanceOffset = MaxDepthDistance - MinDepthDistance;
 
-        //KinectSensor _sensor;
-        /*
-            private void SetupKinectManually()
+        bool closing = false;
+        const int skeletonCount = 6;
+        Skeleton[] allSkeletons = new Skeleton[skeletonCount];
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+          
+        }
+
+        void kinectSensorChooser1_KinectSensorChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            KinectSensor old = (KinectSensor)e.OldValue;
+
+            StopKinect(old);
+
+            KinectSensor sensor = (KinectSensor)e.NewValue;
+
+            if (sensor == null)
             {
+                return;
+            }
 
-                if (KinectSensor.KinectSensors.Count > 0)
+
+
+
+            var parameters = new TransformSmoothParameters
+            {
+                Smoothing = 0.3f,
+                Correction = 0.0f,
+                Prediction = 0.0f,
+                JitterRadius = 1.0f,
+                MaxDeviationRadius = 0.5f
+            };
+            sensor.SkeletonStream.Enable(parameters);
+
+            sensor.SkeletonStream.Enable();
+
+            sensor.AllFramesReady += new EventHandler<AllFramesReadyEventArgs>(sensor_AllFramesReady);
+            sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+            sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+
+            try
+            {
+                sensor.Start();
+            }
+            catch (System.IO.IOException)
+            {
+                kinectSensorChooser1.AppConflictOccurred();
+            }
+        }
+
+        void sensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            if (closing)
+            {
+                return;
+            }
+
+            //Get a skeleton
+            Skeleton first = GetFirstSkeleton(e);
+
+            if (first == null)
+            {
+                return;
+            }
+
+
+
+            //set scaled position
+            //ScalePosition(headImage, first.Joints[JointType.Head]);
+            ScalePosition(leftEllipse, first.Joints[JointType.HandLeft]);
+            ScalePosition(rightEllipse, first.Joints[JointType.HandRight]);
+
+            GetCameraPoint(first, e);
+
+        }
+
+        void GetCameraPoint(Skeleton first, AllFramesReadyEventArgs e)
+        {
+
+            using (DepthImageFrame depth = e.OpenDepthImageFrame())
+            {
+                if (depth == null ||
+                    kinectSensorChooser1.Kinect == null)
                 {
-                    //use first Kinect
-                    _sensor = KinectSensor.KinectSensors[0];
-                            MessageBox.Show("Kinect Status = " + _sensor.Status.ToString()); 
+                    return;
                 }
-                else
+                
+                //Map a joint location to a point on the depth map
+                //head
+                DepthImagePoint headDepthPoint =
+                    depth.MapFromSkeletonPoint(first.Joints[JointType.Head].Position);
+                //left hand
+                DepthImagePoint leftDepthPoint =
+                    depth.MapFromSkeletonPoint(first.Joints[JointType.HandLeft].Position);
+                //right hand
+                DepthImagePoint rightDepthPoint =
+                    depth.MapFromSkeletonPoint(first.Joints[JointType.HandRight].Position);
+
+
+                //Map a depth point to a point on the color image
+                //head
+                ColorImagePoint headColorPoint =
+                    
+                    depth.MapToColorImagePoint(headDepthPoint.X, headDepthPoint.Y,
+                    ColorImageFormat.RgbResolution640x480Fps30);
+                //left hand
+                ColorImagePoint leftColorPoint =
+                    depth.MapToColorImagePoint(leftDepthPoint.X, leftDepthPoint.Y,
+                    ColorImageFormat.RgbResolution640x480Fps30);
+                //right hand
+                ColorImagePoint rightColorPoint =
+                    depth.MapToColorImagePoint(rightDepthPoint.X, rightDepthPoint.Y,
+                    ColorImageFormat.RgbResolution640x480Fps30);
+
+
+                //Set location
+                CameraPosition(headImage, headColorPoint);
+                CameraPosition(leftEllipse, leftColorPoint);
+                CameraPosition(rightEllipse, rightColorPoint);
+            }
+        }
+
+
+        Skeleton GetFirstSkeleton(AllFramesReadyEventArgs e)
+        {
+            using (SkeletonFrame skeletonFrameData = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrameData == null)
                 {
-                    MessageBox.Show("No Kinects are connected"); 
+                    return null;
                 }
 
+
+                skeletonFrameData.CopySkeletonDataTo(allSkeletons);
+
+                //get the first tracked skeleton
+                Skeleton first = (from s in allSkeletons
+                                  where s.TrackingState == SkeletonTrackingState.Tracked
+                                  select s).FirstOrDefault();
+
+                return first;
 
             }
-            */
+        }
 
-
-            private void StopKinect(KinectSensor sensor)
+        private void StopKinect(KinectSensor sensor)
+        {
+            if (sensor != null)
             {
-                if (sensor != null)
+                if (sensor.IsRunning)
                 {
-                    if (sensor.IsRunning)
+                    //stop sensor 
+                    sensor.Stop();
+
+                    //stop audio if not null
+                    if (sensor.AudioSource != null)
                     {
-                        //stop sensor 
-                        sensor.Stop();
-
-                        //stop audio if not null
-                        if (sensor.AudioSource != null)
-                        {
-                            sensor.AudioSource.Stop();
-                        }
-
-
+                        sensor.AudioSource.Stop();
                     }
+
+
                 }
             }
+        }
 
-            private void KinectSensorChooser1_KinectSensorChanged(object sender, DependencyPropertyChangedEventArgs e)
-            {
-                KinectSensor oldSensor = (KinectSensor)e.OldValue;
-
-                StopKinect(oldSensor);
-
-                KinectSensor newSensor = (KinectSensor)e.NewValue;
-
-                if (newSensor == null)
-                {
-                    return;
-                }
-
-                //register for event and enable Kinect sensor features you want
-                newSensor.AllFramesReady += new EventHandler<AllFramesReadyEventArgs>(newSensor_AllFramesReady);
-                newSensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
-                newSensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
-                newSensor.SkeletonStream.Enable();
-
-
-                try
-                {
-                    newSensor.Start();
-                }
-                catch (System.IO.IOException)
-                {
-                    //another app is using Kinect
-                    KinectSensorChooser1.AppConflictOccurred();
-                }
-
-
-
-            }
-
-            void newSensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
-            {
-
-            //depth
-            /*
-            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
-            {
-                if (depthFrame == null)
-                {
-                    return;
-                }
-                byte[] pixels = GenerateColoredBytes(depthFrame);
-
-
-                int stride = depthFrame.Width * 4;
-                image1.Source =
-                    BitmapSource.Create(depthFrame.Width, depthFrame.Height,
-                    96, 96, PixelFormats.Bgr32, null, pixels, stride);
-            }
-            */
-
-            //color
-            using (ColorImageFrame depthFrame = e.OpenColorImageFrame())
-            {
-                if (depthFrame == null)
-                {
-                    return;
-                }
-                byte[] pixels = new byte[depthFrame.PixelDataLength];
-                depthFrame.CopyPixelDataTo(pixels);
-
-                int stride = depthFrame.Width * 4;
-                image1.Source =
-                    BitmapSource.Create(depthFrame.Width, depthFrame.Height,
-                    96, 96, PixelFormats.Bgr32, null, pixels, stride);
-            }
+        private void CameraPosition(FrameworkElement element, ColorImagePoint point)
+        {
+            //Divide by 2 for width and height so point is right in the middle 
+            // instead of in top/left corner
+            Canvas.SetLeft(element, point.X - element.Width / 2);
+            Canvas.SetTop(element, point.Y - element.Height / 2);
 
         }
-        /*
-            private byte[] GenerateColoredBytes(DepthImageFrame depthFrame)
-            {
-                short[] rawDepthData = new short[depthFrame.PixelDataLength];
-                depthFrame.CopyPixelDataTo(rawDepthData);
-                Byte[] pixels = new byte[depthFrame.Height * depthFrame.Width * 4];
-                const int BlueIndex = 0;
-                const int GreenIndex = 1;
-                const int RedIndex = 2;
 
-                for (int depthIndex = 0, colorIndex = 0;
-                    depthIndex < rawDepthData.Length && colorIndex < pixels.Length;
-                    depthIndex++, colorIndex += 4)
-                {
-                    // get the player
-                    int player = rawDepthData[depthIndex] & DepthImageFrame.PlayerIndexBitmask;
+        private void ScalePosition(FrameworkElement element, Joint joint)
+        {
+            //convert the value to X/Y
+            //Joint scaledJoint = joint.ScaleTo(1280, 720); 
 
-                    //get the depth value
-                    int depth = rawDepthData[depthIndex] >> DepthImageFrame.PlayerIndexBitmaskWidth;
+            //convert & scale (.3 = means 1/3 of joint distance)
+            Joint scaledJoint = joint.ScaleTo(1280, 720, .3f, .3f);
 
-                    //0.9M
-                    //if(depth <= 900)
-                    //{
-                    //we are very close
-                    //  pixels[colorIndex + BlueIndex] = 255;
-                    //   pixels[colorIndex + GreenIndex] = 0;
-                    //   pixels[colorIndex + RedIndex] = 0;
-                    // }
-                    // 0.9 -2M 
-                    // else if (depth > 900 && depth < 2000)
-                    // {
-                    // we are at fine distance
-                    //  pixels[colorIndex + BlueIndex] = 0;
-                    //   pixels[colorIndex + GreenIndex] = 255;
-                    //  pixels[colorIndex + RedIndex] = 0;
-                    // }
-                    // +2M
-                    // else if (depth > 2000)
-                    //{
-                    //we are far away
-                    //  pixels[colorIndex + BlueIndex] = 0;
-                    //  pixels[colorIndex + GreenIndex] = 0;
-                    //  pixels[colorIndex + RedIndex] = 255;
-                    // }
-                   // equal coloring for monochromatic histogram
-                    byte intensity = CalculateIntensityFromDepth(depth);
-                    pixels[colorIndex + BlueIndex] = intensity;
-                    pixels[colorIndex + GreenIndex] = intensity;
-                    pixels[colorIndex + RedIndex] = intensity;
+            Canvas.SetLeft(element, scaledJoint.Position.X);
+            Canvas.SetTop(element, scaledJoint.Position.Y);
 
-
-                    //Color all players "gold"
-                    //if (player > 0)
-                    // {
-                    //    pixels[colorIndex + BlueIndex] = Colors.Gold.B;
-                    //    pixels[colorIndex + GreenIndex] = Colors.Gold.G;
-                    //    pixels[colorIndex + RedIndex] = Colors.Gold.R;
-                    //  }
-
-                }
-                return pixels;
-            }
-            */
-
-            public static byte CalculateIntensityFromDepth(int distance)
-            {
-               // formula for calculating monochrome intensity for histogram
-                return (byte)(255 - (255 * Math.Max(distance - MinDepthDistance, 0)
-                    / (MaxDepthDistanceOffset)));
-            }
+        }
         
-            private void Window_Loaded_1(object sender, RoutedEventArgs e)
-            {
-                //SetupKinectManually();
-                KinectSensorChooser1.KinectSensorChanged += new DependencyPropertyChangedEventHandler(KinectSensorChooser1_KinectSensorChanged);
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            kinectSensorChooser1.KinectSensorChanged += new DependencyPropertyChangedEventHandler(kinectSensorChooser1_KinectSensorChanged);
 
-            }
-
-            private void Window_Closing_1(object sender, System.ComponentModel.CancelEventArgs e)
-            {
-                StopKinect(KinectSensorChooser1.Kinect);
-            }
         }
 
+        private void Window_Closing_1(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            closing = true;
+            StopKinect(kinectSensorChooser1.Kinect);
+        }
     }
+}
